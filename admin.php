2cdda8833792f9213e18727b423de82d00c6a899
NathanFaucon <97155987+NathanFaucon@users.noreply.github.com>
2022-11-15 13:31:46 +0100

    Update and rename admin.html to admin.php

--- admin.php
+++ admin.php
@@ -6,6 +6,9 @@
     <link rel="stylesheet" href="style.css">
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
+
+<?php require 'auth.php';?>
+    
 <body>
 
 <div class="absolute top-8 flex justify-between w-full px-8 items-center">
