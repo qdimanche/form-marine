bf123793e052f8a9bbe066690b74282938d22a1c
SerialFrostiz <jeremy.mikolajczak@laposte.net>
2022-11-15 14:25:17 +0100

    delete gitignore + delete connect

--- config/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-try {
-    $db = new PDO('mysql:host=localhost;dbname=formmarine', 'root', '');
-} catch (PDOException $e) {
-    print "Error! : " . $e->getMessage() . "<br/>";
-    die();
-}
