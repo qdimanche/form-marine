a983a737610af9f571f7143bf38a0a7877d3956f
quentindimanche <qdimanche@gmail.com>
2022-11-15 14:18:41 +0100

    [+] foreach index

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php
+require_once './controller/connect.php';
+require_once './controller/getAllQuestions.php';
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,29 +14,6 @@
 <body>
 
 
-<div class="">
-
-
-    <form method="post" action="" class="flex justify-center min-h-screen items-center">
-        <div class="flex flex-col space-y-2">
-            <span>What is your favourite color</span>
-
-            <div class="flex space-x-2">
-                <input type="radio" id="response1" name="question1" value="valeur-bareme">
-                <label for="response1"> I have a bike</label>
-            </div>
-            <div class="flex space-x-2">
-                <input type="radio" id="response2"  name="question1" value="valeur-bareme">
-                <label for="response2"> I have a bike</label>
-            </div>
-            <div class="flex space-x-2">
-                <input type="radio" id="response3" name="question1" value="valeur-bareme">
-                <label for="response3"> I have a bike</label>
-            </div>
-            <button type="submit" class="bg-[#3D5A80]/80 duration-300 hover:bg-[#3D5A80] text-white px-2 py-3 rounded-[10px] text-sm w-full !mt-6">Envoyer</button>
-        </div>
-    </form>
-
     <div class="absolute top-8 flex justify-between w-full px-8 items-center">
         <a href="index.php"><img src="assets/images/home.png" class="w-6" alt=""></a>
         <div class="space-x-2">
@@ -38,7 +21,41 @@
             <a href="loginForm.php" class="bg-[#293241]/80 duration-300 hover:bg-[#293241] text-white px-2 py-3 rounded-[10px] text-sm">Se connecter</a>
         </div>
     </div>
-</div>
+
+
+    <?php
+
+    foreach ($res as $question) {
+        echo ' 
+            <form method="post" action="" class="flex justify-center min-h-screen items-center">
+                <div class="flex flex-col space-y-2">
+                    <span>'. $question['name']. '</span>
+        
+                    <div class="flex space-x-2">
+                        <input type="radio" id="response1" name="question' . $question['answer1'] . '" value="valeur-bareme">
+                        <label for="response1"> I have a bike</label>
+                    </div>
+                    <div class="flex space-x-2">
+                        <input type="radio" id="response2"  name="question' . $question['answer2'] . '" value="valeur-bareme">
+                        <label for="response2"> I have a bike</label>
+                    </div>
+                    <div class="flex space-x-2">
+                        <input type="radio" id="response3" name="question' . $question['answer3'] . '"  value="valeur-bareme">
+                        <label for="response3"> I have a bike</label>
+                    </div>
+                    <div class="flex space-x-2">
+                        <input type="radio" id="response3" name="question' . $question['answer4'] . '"  value="valeur-bareme">
+                        <label for="response3"> I have a bike</label>
+                    </div>
+                    <button type="submit" class="bg-[#3D5A80]/80 duration-300 hover:bg-[#3D5A80] text-white px-2 py-3 rounded-[10px] text-sm w-full !mt-6">Envoyer</button>
+                </div>
+            </form>
+        
+        
+        ';    } ?>
+
+
+
 
 
 
