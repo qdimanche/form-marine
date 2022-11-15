0bec890db3a559a28855e4fe17cee732d6b2f5b1
NathanFaucon <97155987+NathanFaucon@users.noreply.github.com>
2022-11-15 11:34:04 +0100

    Add files via upload

--- auth.php
+++ auth.php
@@ -12,7 +12,7 @@ use \Firebase\JWT\Key;
 $jwt = null;
 if (isset($_COOKIE['JWT'])) {
     $jwt = trim($_COOKIE['JWT']);    
-    $token = JWT::decode($jwt, new Key($tokenKey, 'HS256'));
+    $token = JWT::decode($jwt, new Key('CESI', 'HS256'));
     $token = (array) $token;
     if($token['isConnected'] == true) {
         $_SESSION['login'] = $token['login'];
@@ -30,7 +30,7 @@ if (isset($_COOKIE['JWT'])) {
     }                         
 }else{
     $_SESSION['redirectStatus'] = "InvalidToken";  
-    header('Location: ../front/index.php');
+    header('Location: ./index.php');
     die();
 }
 
