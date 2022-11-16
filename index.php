a20203d12319b13a9888ec2f0ff2d7e480e04a07
Nathan <nathan.faucon@viacesi.fr>
2022-11-16 14:53:54 +0100

    push nathan

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ require_once __DIR__.'/controller/getAllQuestions.php';
     <?php require 'header.php';?>
 
 
-    <form method="post" action="./controller/createResult.php" class="flex flex-col space-y-6 w-1/4 mx-auto pt-32 pb-16">
+    <form method="post" onsubmit="return verifRadio()" action="./controller/createResult.php" class="flex flex-col space-y-6 w-1/4 mx-auto pt-32 pb-16">
     <?php
 
     foreach ($res as $question):
@@ -46,14 +46,15 @@ require_once __DIR__.'/controller/getAllQuestions.php';
                 </div>
 
             </div>
-          <?php else: ?> <input type="hidden" name="nbQuestion" value="<?php echo $question ?>">
+          <?php else: ?> <input type="hidden" id="nbQuestion" name="nbQuestion" value="<?php echo $question ?>">
                   <?php endif; endforeach ;?>
 
-        <button type="submit" class="bg-[#3D5A80]/80 duration-300 hover:bg-[#3D5A80] text-white px-2 py-3 rounded-[10px] text-sm w-full !mt-6 w-1/4 items-center">Envoyer</button>
+        <button type="submit" id="btn_Questions" class="bg-[#3D5A80]/80 duration-300 hover:bg-[#3D5A80] text-white px-2 py-3 rounded-[10px] text-sm w-full !mt-6 w-1/4 items-center">Envoyer</button>
 
     </form>
 
 
+    <script src='scriptQuestions.js'></script>
 
 
 
