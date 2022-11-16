c8edd39cec9974014bb551acc3e4b89ea0331357
Nathan <nathan.faucon@viacesi.fr>
2022-11-16 11:28:31 +0100

    connexion fonctionnelle

--- loginForm.php
+++ loginForm.php
@@ -11,13 +11,7 @@
 </head>
 <body>
 
-<div class="absolute top-8 flex justify-between w-full px-8 items-center">
-    <a href="index.php"><img src="assets/images/home.png" class="w-6" alt=""></a>
-    <div class="space-x-2">
-        <a href="admin.php" class="bg-[#EE6C4D]/80 duration-300 hover:bg-[#EE6C4D] text-white px-2 py-3 rounded-[10px] text-sm">Administration</a>
-        <a href="loginForm.php" class="bg-[#293241]/80 duration-300 hover:bg-[#293241] text-white px-2 py-3 rounded-[10px] text-sm">Se connecter</a>
-    </div>
-</div>
+<?php require 'header.php';?>
 
 <form action="createToken.php" method="post" class="space-y-6 flex flex-col min-h-screen mx-auto justify-center w-fit">
     <div class="flex flex-col space-y-2">
