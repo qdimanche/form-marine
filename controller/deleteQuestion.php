e924f6aafdbc4f233d2683973ae9abf8442c2980
quentindimanche <qdimanche@gmail.com>
2022-11-17 16:27:47 +0100

    [+] adminGateway

--- controller/deleteQuestion.php
+++ controller/deleteQuestion.php
@@ -1,15 +1,14 @@
 <?php
 require dirname(__DIR__).'/config/connect.php';
 $questionId = $_POST['questionID'];
-//var_dump($_POST);
 
 $query = "DELETE FROM `answers` WHERE questionID = '$questionId'; DELETE FROM questions WHERE questionID = '$questionId'; ";
 $stmt = $db->prepare($query);
-var_dump($query);
 try {
     $stmt = $stmt->execute();
 }
 catch (PDOException $e) {
     echo 'Error : ' . $e->getMessage() . '<br/>';
 }
+
 header('Location: ../questionsList.php');
--- header.php
+++ header.php
@@ -3,8 +3,7 @@
         <a href="index.php"><img src="assets/images/home.png" class="w-6" alt=""></a>
         <div class="space-x-4">
         <?php if(isset($_SESSION['isConnected']) && $_SESSION['isConnected'] && isset($_COOKIE['JWT'])) {
-            echo '<a href="questionsList.php" class="bg-[#EE6C4D]/80 duration-300 hover:bg-[#EE6C4D] text-white px-2 py-3 rounded-[10px] text-sm">Administration</a>';
-            echo '<a href="resultsList.php" class="bg-[#EE6C4D]/80 duration-300 hover:bg-[#EE6C4D] text-white px-2 py-3 rounded-[10px] text-sm">Résultats</a>';
+            echo '<a href="adminGateway.php" class="bg-[#EE6C4D]/80 duration-300 hover:bg-[#EE6C4D] text-white px-2 py-3 rounded-[10px] text-sm">Administration</a>';
             echo '<a href="logOut.php" class="bg-[#293241]/80 duration-300 hover:bg-[#293241] text-white px-2 py-3 rounded-[10px] text-sm">Se déconnecter</a>';
         }
         else{
