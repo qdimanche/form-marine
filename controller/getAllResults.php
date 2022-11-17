81812ae5d6ce29e811d8f0e81ba26ec0e35963d9
SerialFrostiz <jeremy.mikolajczak@laposte.net>
2022-11-17 11:22:59 +0100

    edit getAllResults

--- controller/getAllResults.php
+++ controller/getAllResults.php
@@ -1,12 +1,19 @@
 <?php
 
 require dirname(__DIR__).'/config/connect.php';
-
-$stmt = $db->prepare("Select `average` from results");
+$res = [];
+$iterator = 0;
+$stmt = $db->prepare("Select * from results");
 try {
     $stmt->execute();
 } catch (PDOException $e) {
     print "Error! : " . $e->getMessage() . "<br/>";
     die();
 }
+$data = $stmt->fetchAll();
+foreach($data as $result){
+    $res[$iterator] = $result['average'];
+    $iterator++;
+}
+var_dump($res);
 ?>
