50ea3cce1e9b9ed8945a46ff98cead80f0bb3351
quentindimanche <qdimanche@gmail.com>
2022-11-17 12:01:44 +0100

    [+] style: header

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <?php session_start(); ?>
 <div class="absolute top-8 flex justify-between w-full px-8 items-center">
         <a href="index.php"><img src="assets/images/home.png" class="w-6" alt=""></a>
-        <div class="space-x-2">
+        <div class="space-x-4">
         <?php if(isset($_SESSION['isConnected']) && $_SESSION['isConnected'] && isset($_COOKIE['JWT'])) {
             echo '<a href="admin.php" class="bg-[#EE6C4D]/80 duration-300 hover:bg-[#EE6C4D] text-white px-2 py-3 rounded-[10px] text-sm">Administration</a>';
             echo '<a href="resultsList.php" class="bg-[#EE6C4D]/80 duration-300 hover:bg-[#EE6C4D] text-white px-2 py-3 rounded-[10px] text-sm">Résultats</a>';
